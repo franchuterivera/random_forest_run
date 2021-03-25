18a91164fca75a64612e68a6cbd2684bf2c698fc
chico <francisco.rivera.valverde@gmail.com>
2021-03-25 13:46:22 +0100

    Try missing cmake packages

--- build_tools/env.sh
+++ build_tools/env.sh
@@ -4,13 +4,17 @@ set -e
 set -x
 
 # Install the environment requirements
+sudo apt-get -qq update
 sudo apt-get install -y build-essential
+sudo apt-get install -y cmake
+sudo apt-get install -y libblkid-dev
+sudo apt-get install -y e2fslibs-dev
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y libaudit-dev
+sudo apt-get install -y swig
+sudo apt-get install -y lcov
+sudo apt-get install -y doxygen
 python3 -m pip install --upgrade pip
 pip3 install "numpy<=1.19"
-sudo apt-get -qq update
-sudo apt-get install -y libboost-all-dev
-sudo apt-get install swig
 sudo gem install coveralls-lcov
-sudo apt-get install -y lcov
-sudo apt-get install doxygen
 pip3 install --user -U pip-tools
