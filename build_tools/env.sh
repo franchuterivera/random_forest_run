b52c84a1600647e855a13b3a9786935466bec197
chico <francisco.rivera.valverde@gmail.com>
2021-03-25 12:57:43 +0100

    FIX_57

--- build_tools/env.sh
+++ build_tools/env.sh
@@ -5,13 +5,11 @@ set -x
 
 # Install the environment requirements
 sudo apt-get install -y build-essential
-python -m pip install --upgrade pip
-pip install "numpy<=1.19"
+python3 -m pip install --upgrade pip
+pip3 install "numpy<=1.19"
 sudo apt-get -qq update
 sudo apt-get install -y libboost-all-dev
-sudo apt-get remove swig
-sudo apt-get -y install swig3.0
-sudo ln -s /usr/bin/swig3.0 /usr/bin/swig
+sudo apt-get install swig
 sudo gem install coveralls-lcov
 sudo apt-get install -y lcov
 sudo apt-get install doxygen
